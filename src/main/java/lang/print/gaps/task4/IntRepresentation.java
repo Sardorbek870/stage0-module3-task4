20f8933ffb2a37b08151697b5c66507b7b544ad3
Stas Sukora <st.sukora@gmail.com>
2022-11-22 00:44:47 +0300

    module3Task4

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char[] array = {'c','h','a','r'};
+        for(char element : array){
+            int code = (int) element;
+            System.out.println(code);
+        }
     }
 }
